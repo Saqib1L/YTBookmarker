56eebf0beb08a6565769893d56fc94a09bca77dc
Saqib1L <muhammadsaqibka@gmail.com>
2026-05-12 18:07:37 +0800

    add icon + change name + add more to js

--- popup.js
+++ popup.js
@@ -1,138 +1,153 @@
 //Theme Toggle
 function applyTheme(theme) {
-  popup =  document.getElementById('popup');
-  popup.setAttribute('data-theme', theme);
+  const popup = document.getElementById("popup");
+  popup.setAttribute("data-theme", theme);
 
-  if(theme === 'dark') {
-    document.getElementById('icon-sun').style.display = 'block';
-    document.getElementById('icon-moon').style.display = 'none';
+  if (theme === "dark") {
+    document.getElementById("icon-sun").style.display = "block";
+    document.getElementById("icon-moon").style.display = "none";
   } else {
-    document.getElementById('icon-sun').style.display = 'none';
-    document.getElementById('icon-moon').style.display = 'block';
+    document.getElementById("icon-sun").style.display = "none";
+    document.getElementById("icon-moon").style.display = "block";
   }
 }
 
-document.getElementById('theme-toggle').addEventListener('click', () => {
-  let currentTheme = popup.getAttribute('data-theme');
+document.getElementById("theme-toggle").addEventListener("click", () => {
+  let currentTheme = popup.getAttribute("data-theme");
   let newTheme;
 
-  if(currentTheme === 'dark') {
-    newTheme = 'light';
+  if (currentTheme === "dark") {
+    newTheme = "light";
   } else {
-    newTheme = 'dark';
+    newTheme = "dark";
   }
 
   applyTheme(newTheme);
-   chrome.storage.local.set({theme: newTheme})
+  chrome.storage.local.set({ theme: newTheme });
 });
 
-chrome.storage.local.get('theme', (result) => {
-  if(result.theme) {
+chrome.storage.local.get("theme", (result) => {
+  if (result.theme) {
     applyTheme(result.theme);
   } else {
-    applyTheme('light');
+    applyTheme("light");
   }
-})
+});
+
+//Shortcuts
+const input = document.getElementById("search-input");
+
+window.addEventListener("keydown", (e) => {
+  // Ignore if already typing
+  if (
+    document.activeElement.tagName === "INPUT" ||
+    document.activeElement.tagName === "TEXTAREA"
+  ) {
+    return;
+  }
+
+  if (e.key === "/") {
+    e.preventDefault();
+    input.focus();
+  }
+});
 
 //Generate Categoires Dynamically
 function renderCategories(categories) {
-  const list = document.getElementById('categories-list');
-  list.innerHTML = '';
+  const list = document.getElementById("categories-list");
+  list.innerHTML = "";
 
   categories.forEach((category) => {
-    const button = document.createElement('button');
-    const wrapper = document.createElement('div');
+    const button = document.createElement("button");
+    const wrapper = document.createElement("div");
 
-    button.className = 'category-item';
+    button.className = "category-item";
     button.textContent = category;
-    button.setAttribute('data-category', category);
+    button.setAttribute("data-category", category);
 
-    wrapper.className = 'category-item-wrapper';
+    wrapper.className = "category-item-wrapper";
 
-    const dotsBtn = document.createElement('button');
-    dotsBtn.className = 'dots-btn';
-    dotsBtn.textContent = '⋮';
+    const dotsBtn = document.createElement("button");
+    dotsBtn.className = "dots-btn";
+    dotsBtn.textContent = "⋮";
 
+    dotsBtn.addEventListener("click", (event) => {
+      const categoryDetailMenu = document.createElement("div");
+      categoryDetailMenu.className = "category-detail-menu";
 
-    dotsBtn.addEventListener('click', (event) => {
-      const categoryDetailMenu = document.createElement('div');
-      categoryDetailMenu.className = 'category-detail-menu';
+      const categoryDeleteButton = document.createElement("button");
+      const categoryRenameButton = document.createElement("button");
 
-      const categoryDeleteButton = document.createElement('button');
-      const categoryRenameButton = document.createElement('button');
-    
-      categoryDeleteButton.textContent = 'Delete';
-      categoryRenameButton.textContent = 'Rename';
-      categoryDeleteButton.className = 'category-menu-delete-button';
-      categoryRenameButton.className = 'category-menu-rename-button';
+      categoryDeleteButton.textContent = "Delete";
+      categoryRenameButton.textContent = "Rename";
+      categoryDeleteButton.className = "category-menu-delete-button";
+      categoryRenameButton.className = "category-menu-rename-button";
 
       categoryDetailMenu.appendChild(categoryDeleteButton);
       categoryDetailMenu.appendChild(categoryRenameButton);
 
-      document.addEventListener('click', (e) => {
-        if(!wrapper.contains(e.target)) {
+      document.addEventListener("click", (e) => {
+        if (!wrapper.contains(e.target)) {
           categoryDetailMenu.remove();
         }
       });
 
-         wrapper.appendChild(categoryDetailMenu);
+      wrapper.appendChild(categoryDetailMenu);
     });
 
-     wrapper.appendChild(button);
+    wrapper.appendChild(button);
 
-    if(category !== 'All') {
+    if (category !== "All") {
       wrapper.appendChild(dotsBtn);
     }
 
-    if(category === 'All') {
-      button.classList.add('active');
+    if (category === "All") {
+      button.classList.add("active");
     }
 
     list.appendChild(wrapper);
-  }); 
+  });
 }
 
-chrome.storage.local.get('categories', (result) => {
+chrome.storage.local.get("categories", (result) => {
   if (result.categories) {
     renderCategories(result.categories);
   } else {
-    renderCategories(['All']);
+    renderCategories(["All"]);
   }
-})
+});
 
-document.getElementById('new-category-btn').addEventListener('click', () => {
-  chrome.storage.local.get('categories', (result) => {  
-    
-    const categories = result.categories || ['All'];
-    const nonAllCategories = categories.filter((category) => category !== 'All');
+document.getElementById("new-category-btn").addEventListener("click", () => {
+  chrome.storage.local.get("categories", (result) => {
+    const categories = result.categories || ["All"];
+    const nonAllCategories = categories.filter(
+      (category) => category !== "All",
+    );
     const count = nonAllCategories.length;
-    const newName = 'Category ' + (count + 1);
-  
-    categories.push(newName);
-    chrome.storage.local.set({categories: categories});
+    const newName = "Category " + (count + 1);
 
-    const inputElement = document.createElement('input');
-    inputElement.className = 'category-input';
+    categories.push(newName);
+    chrome.storage.local.set({ categories: categories });
+
+    const inputElement = document.createElement("input");
+    inputElement.className = "category-input";
     inputElement.value = newName;
-    document.getElementById('categories-list').appendChild(inputElement);
+    document.getElementById("categories-list").appendChild(inputElement);
     inputElement.select();
 
-    inputElement.addEventListener('keydown', logKey);
+    inputElement.addEventListener("keydown", logKey);
     function logKey(e) {
-     if(e.key === 'Enter') {
+      if (e.key === "Enter") {
         categories[categories.length - 1] = inputElement.value || newName;
-        chrome.storage.local.set({categories: categories});
+        chrome.storage.local.set({ categories: categories });
         renderCategories(categories);
-     }
+      }
     }
 
-    inputElement.addEventListener('blur', () => {
+    inputElement.addEventListener("blur", () => {
       categories[categories.length - 1] = inputElement.value || newName;
-      chrome.storage.local.set({categories: categories});
+      chrome.storage.local.set({ categories: categories });
       renderCategories(categories);
     });
   });
 });
-
-
-
