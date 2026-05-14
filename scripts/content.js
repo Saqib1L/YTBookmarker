69c8746acacb93f7c2a6e27a678536b5804453d1
Saqib1L <muhammadsaqibka@gmail.com>
2026-05-14 18:32:06 +0800

    Initialize content.js to inject in js

---
+++ scripts/content.js
@@ -0,0 +1,7 @@
+const addBookmarkButton = document.createElement('button');
+addBookmarkButton.textContent = '+';
+addBookmarkButton.className = 'add-bookmark-button';
+
+
+const autplayButton = document.querySelector('.ytp-autonav-toggle-button');
+//autplayButton.insertAdjacentElement('beforebegin', addBookmarkButton);
