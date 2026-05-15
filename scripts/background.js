fc0852cb4b7521c417345d8fce0510e536c562ea
Saqib1L <muhammadsaqibka@gmail.com>
2026-05-15 17:42:41 +0800

    Fix the damn loading bug (took 2 hours)

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,12 @@
 chrome.action.onClicked.addListener((tab) => {
   chrome.sidePanel.open({ windowId: tab.windowId });
 });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && tab.url?.includes('youtube.com/watch')) {
+    chrome.scripting.executeScript({
+      target: { tabId },
+      files: ['scripts/content.js']
+    });
+  }
+});
--- scripts/content.js
+++ scripts/content.js
@@ -1,13 +1,13 @@
 function injectButton() {
-  const rightControls = document.querySelector('.ytp-right-controls');
+  if (document.querySelector('.add-bookmark-button')) return;
   
-  if(!rightControls) return;
-  if(document.querySelector('.add-bookmark-button')) return;
+  const rightControls = document.querySelector('.ytp-right-controls');
+  if (!rightControls) return;
 
   const addBookmarkButton = document.createElement('button');
   addBookmarkButton.className = 'ytp-button add-bookmark-button';
-  addBookmarkButton.style.backgroundImage = `url(${chrome.runtime.getURL('icons/bookmark-icon.png')})`
-
+  addBookmarkButton.title = 'Add Bookmark';
+  addBookmarkButton.style.backgroundImage = `url(${chrome.runtime.getURL('icons/bookmark-icon.png')})`;
   addBookmarkButton.style.backgroundSize = '24px';
   addBookmarkButton.style.backgroundPosition = 'center';
   addBookmarkButton.style.backgroundRepeat = 'no-repeat';
@@ -15,7 +15,4 @@ function injectButton() {
   rightControls.prepend(addBookmarkButton);
 }
 
-document.addEventListener('yt-navigate-finish', injectButton);
 injectButton();
-
-//fix new yt loading issue
