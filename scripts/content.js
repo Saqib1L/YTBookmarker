1ec2cfa2b537f974b42ed807eaf47391bd7ed06f
Saqib1L <muhammadsaqibka@gmail.com>
2026-05-15 11:09:44 +0800

    inject addbookmark button in youtube controls bar

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,21 @@
-const addBookmarkButton = document.createElement('button');
-addBookmarkButton.textContent = '+';
-addBookmarkButton.className = 'add-bookmark-button';
+function injectButton() {
+  const rightControls = document.querySelector('.ytp-right-controls');
+  
+  if(!rightControls) return;
+  if(document.querySelector('.add-bookmark-button')) return;
 
+  const addBookmarkButton = document.createElement('button');
+  addBookmarkButton.className = 'ytp-button add-bookmark-button';
+  addBookmarkButton.style.backgroundImage = `url(${chrome.runtime.getURL('icons/bookmark-icon.png')})`
 
-const autplayButton = document.querySelector('.ytp-autonav-toggle-button');
-//autplayButton.insertAdjacentElement('beforebegin', addBookmarkButton);
+  addBookmarkButton.style.backgroundSize = '24px';
+  addBookmarkButton.style.backgroundPosition = 'center';
+  addBookmarkButton.style.backgroundRepeat = 'no-repeat';
+
+  rightControls.prepend(addBookmarkButton);
+}
+
+document.addEventListener('yt-navigate-finish', injectButton);
+injectButton();
+
+//fix new yt loading issue
