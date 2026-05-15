2a0d5ad06352d65629d7da1e3871042f2c92ab51
Saqib1L <muhammadsaqibka@gmail.com>
2026-05-16 07:57:36 +0800

    organize css, add the initial steps of the addbookmark function

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,5 @@
+import { getStorage, setStorage } from "./storage.js";
+
 function injectButton() {
   if (document.querySelector('.add-bookmark-button')) return;
   
@@ -13,6 +15,45 @@ function injectButton() {
   addBookmarkButton.style.backgroundRepeat = 'no-repeat';
 
   rightControls.prepend(addBookmarkButton);
+  return addBookmarkButton;
 }
 
-injectButton();
+const addBookmarkButton = injectButton();
+
+addBookmarkButton.addEventListener('click', () => {
+  const videoData = {
+     videoTitle: document.querySelector('h1.ytd-watch-metadata yt-formatted-string')?.textContent,
+     channelName: document.querySelector('#channel-name a')?.textContent,
+     videoUrl: window.location.href.split('&')[0],
+     timestamp: Math.floor(document.querySelector('video')?.currentTime || 0),
+    }
+});
+
+function createSaveModal(videoData) {
+  const bookmarkModal = document.createElement('div');
+  bookmarkModal.className = 'yt-bookmarker-overlay';
+
+  const bookmarkCustomName = document.createElement('input');
+  bookmarkCustomName.className = 'bookmark-custom-name';
+  bookmarkCustomName.value = videoData.videoTitle;
+
+  const bookmarkCategory = document.createElement('select');
+  bookmarkCategory.className = 'bookmark-category';
+
+  const bookmarkSaveButton = document.createElement('button');
+  bookmarkSaveButton.className = 'bookmark-save-button';
+  bookmarkSaveButton.textContent = 'Save';
+  
+  const bookmarkCancelButton = document.createElement('button');
+  bookmarkCancelButton.className = 'bookmark-cancel-button';
+  bookmarkSaveButton.textContent = 'Cancel';
+
+  bookmarkModal.appendChild(bookmarkCustomName);
+  bookmarkModal.appendChild(bookmarkCategory);
+  bookmarkModal.appendChild(bookmarkSaveButton);
+  bookmarkModal.appendChild(bookmarkCancelButton);
+
+  document.querySelector('.html5-video-player').appendChild(bookmarkModal);
+
+}
+
