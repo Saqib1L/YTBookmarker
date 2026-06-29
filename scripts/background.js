1ac484741228ffd0f17e423e4373f7a95d992f93
Saqib1L <muhammadsaqibka@gmail.com>
2026-06-29 18:17:47 +0800

    remove dead code

--- scripts/background.js
+++ scripts/background.js
@@ -1,9 +1,3 @@
-/*
-chrome.action.onClicked.addListener((tab) => {
-  chrome.sidePanel.open({ windowId: tab.windowId });
-});
-*/
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete' && tab.url?.includes('youtube.com/watch')) {
     chrome.scripting.executeScript({
